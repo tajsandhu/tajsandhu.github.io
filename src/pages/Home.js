ee57dcc501e07d921248d2358f4a49abf8a2db77
Tajbir Sandhu <sandman.taj@gmail.com>
2020-03-08 15:19:42 -0700

    Made some suick changes to the message view

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,29 +23,23 @@ class Home extends React.Component {
                     <div className='Message-box'>
                         <h1 className='Main-title'>Send Message</h1>
                         <div className='Vertical-field'>
-                            <label className='Label-text'>Full Name</label>
                             <div className='Horizontal-field'>
                                 <div className='Vertical-field' style={{paddingRight: 2}}>
-                                    <TextField color='secondary' variant='outlined' size='small'/>
-                                    <label className='Sub-text'>First Name</label>
+                                    <TextField color='secondary' variant='outlined' size='small' label='First Name'/>
                                 </div>
                                 <div className='Vertical-field' style={{paddingLeft: 2}}>
-                                    <TextField color='secondary' variant='outlined' size='small'/>
-                                    <label className='Sub-text'>Last Name</label>
+                                    <TextField color='secondary' variant='outlined' size='small' label='Last Name'/>
                                 </div>
                             </div>
                         </div>
                         <div className='Vertical-field'>
-                            <label className='Label-text'>Email</label>
-                            <TextField color='secondary' variant='outlined' size='small'/>
+                            <TextField color='secondary' variant='outlined' size='small' label='Email'/>
                         </div>
                         <div className='Vertical-field'>
-                            <label className='Label-text'>Company</label>
-                            <TextField color='secondary' variant='outlined' size='small'/>
+                            <TextField color='secondary' variant='outlined' size='small' label='Company'/>
                         </div>
                         <div className='Vertical-field'>
-                            <label className='Label-text'>Message</label>
-                            <TextField color='secondary' variant='outlined' size='small' multiline={true} rows={5}/>
+                            <TextField color='secondary' variant='outlined' size='small' multiline={true} rows={5} label='Message'/>
                         </div>
                         <Button className='Button'  color='secondary' variant='outlined' style={{alignSelf: 'center'}}>Send</Button>
                     </div>
