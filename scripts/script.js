37ddb82259edb7c74cf858568058a43dc1b46360
Taj <sandhu_tajbir@outlook.com>
2020-08-22 14:26:56 -0700

    Fixed animation issues for mobile menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,20 @@
-var menuButton = document.body.querySelector("#mobile-menu-button");
+var menuButton = document.body.querySelector(".mobile-menu-button");
 var siteCanvas = document.body.querySelector(".site");
 
+siteCanvas.classList.toggle("reveal-menu");
+menuButton.classList.toggle("shift-button");
 
 menuButton.addEventListener("click", () => {
+    siteCanvas.style.transition = "left .7s";
+    menuButton.style.transition = "right .7s";
+    siteCanvas.classList.toggle("reveal-menu");
+    menuButton.classList.toggle("shift-button");
     if (siteCanvas.style.left != "-250px") {
-        siteCanvas.style.left = "-250px";
+        siteCanvas.style.overflowX = "initial";
     } else {
         siteCanvas.style.left = "0";
+        setTimeout(() => {
+            siteCanvas.style.overflowX = "hidden";
+        }, 700);
     }
 });
