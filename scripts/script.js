545013f7c3714a3d84dc73a78756116b1de1ea3e
Taj <sandhu_tajbir@outlook.com>
2020-08-22 18:44:20 -0700

    fixes another bug with the mobile menu, adds large screen styles

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,21 @@
 var menuButton = document.body.querySelector(".mobile-menu-button");
 var siteCanvas = document.body.querySelector(".site");
 
-siteCanvas.classList.toggle("reveal-menu");
-menuButton.classList.toggle("shift-button");
+siteCanvas.classList.remove("reveal-menu");
+menuButton.classList.remove("shift-button");
 
 menuButton.addEventListener("click", () => {
-    siteCanvas.style.transition = "left .7s";
-    menuButton.style.transition = "right .7s";
-    siteCanvas.classList.toggle("reveal-menu");
-    menuButton.classList.toggle("shift-button");
-    if (siteCanvas.style.left != "-250px") {
-        siteCanvas.style.overflowX = "initial";
-    } else {
-        siteCanvas.style.left = "0";
+    siteCanvas.style.transition = "left .7s ease";
+    menuButton.style.transition = "right .7s ease";
+    if (siteCanvas.classList.contains("reveal-menu") == true) {    
+        menuButton.classList.remove("shift-button");
+        siteCanvas.classList.remove("reveal-menu");
         setTimeout(() => {
             siteCanvas.style.overflowX = "hidden";
         }, 700);
+    } else {
+        siteCanvas.style.overflowX = "initial";
+        siteCanvas.classList.add("reveal-menu");
+        menuButton.classList.add("shift-button");
     }
 });
