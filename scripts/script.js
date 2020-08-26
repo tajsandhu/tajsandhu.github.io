3387cee6fadc5e50d92d6eb9c9b2d2370a224188
Taj <sandhu_tajbir@outlook.com>
2020-08-26 13:57:03 -0700

    fixes potential issue with mobile navigation

--- scripts/script.js
+++ scripts/script.js
@@ -3,18 +3,25 @@ var menuButton = document.body.querySelector(".mobile-menu-button");
 var siteCanvas = document.body.querySelector(".site");
 var mainContent = document.body.querySelector(".content-wrapper");
 
+
 // initially hides the sidebar
+siteCanvas.style.overflowX = "hidden";
 siteCanvas.classList.add("hide-menu");
 menuButton.classList.remove("shift-button");
+
 
 // function to hide the sidebar and shift the menu button to the default location
 function hideSideBar() { 
     siteCanvas.classList.add("hide-menu");
     menuButton.classList.remove("shift-button");
+    setTimeout(() => {
+        siteCanvas.style.overflowX = "hidden";
+    }, 700);
 }
 
 // function to show the sidebar and shift the button to the sidebar open position
 function showSideBar() {
+    siteCanvas.style.overflowX = "initial";
     siteCanvas.classList.remove("hide-menu");
     menuButton.classList.add("shift-button");
 }
@@ -24,9 +31,17 @@ menuButton.addEventListener("click", () => {
     siteCanvas.style.transition = "left .7s ease";
     menuButton.style.transition = "right .7s ease";
     if (siteCanvas.classList.contains("hide-menu") == true) { 
+        menuButton.disabled = true;
         showSideBar();
+        setTimeout(() => {
+            menuButton.disabled = false;
+        }, 700);
     } else {
+        menuButton.disabled = true;
         hideSideBar();
+        setTimeout(() => {
+            menuButton.disabled = false;
+        }, 700); 
     }
 });
 
